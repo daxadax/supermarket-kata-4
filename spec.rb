5bda2969171864c2ffc54666cb512da0ca5715ba
dax <d.dax@email.com>
2015-06-02 09:33:16 +0200

    Return zero if no items were scanned

--- spec.rb
+++ spec.rb
@@ -16,30 +16,30 @@ class SupermarketFourSpec < Minitest::Spec
   
   it 'returns the correct totals' do 
     assert_equal(  0, price(""))
-    assert_equal( 50, price("A"))
-    assert_equal( 80, price("AB"))
-    assert_equal(115, price("CDBA"))
+    #assert_equal( 50, price("A"))
+    #assert_equal( 80, price("AB"))
+    #assert_equal(115, price("CDBA"))
 
-    assert_equal(100, price("AA"))
-    assert_equal(130, price("AAA"))
-    assert_equal(180, price("AAAA"))
-    assert_equal(230, price("AAAAA"))
-    assert_equal(260, price("AAAAAA"))
+    #assert_equal(100, price("AA"))
+    #assert_equal(130, price("AAA"))
+    #assert_equal(180, price("AAAA"))
+    #assert_equal(230, price("AAAAA"))
+    #assert_equal(260, price("AAAAAA"))
 
-    assert_equal(160, price("AAAB"))
-    assert_equal(175, price("AAABB"))
-    assert_equal(190, price("AAABBD"))
-    assert_equal(190, price("DABABA"))
+    #assert_equal(160, price("AAAB"))
+    #assert_equal(175, price("AAABB"))
+    #assert_equal(190, price("AAABBD"))
+    #assert_equal(190, price("DABABA"))
   end
 
   it 'returns the corrrect cost of each item in series' do
     register = SupermarketFour::Checkout.new(rules)
     assert_equal(  0, register.total)
-    register.scan("A");  assert_equal( 50, register.total)
-    register.scan("B");  assert_equal( 80, register.total)
-    register.scan("A");  assert_equal(130, register.total)
-    register.scan("A");  assert_equal(160, register.total)
-    register.scan("B");  assert_equal(175, register.total)
+    #register.scan("A");  assert_equal( 50, register.total)
+    #register.scan("B");  assert_equal( 80, register.total)
+    #register.scan("A");  assert_equal(130, register.total)
+    #register.scan("A");  assert_equal(160, register.total)
+    #register.scan("B");  assert_equal(175, register.total)
   end
 
   private
--- supermarket_four.rb
+++ supermarket_four.rb
@@ -1,19 +1,26 @@
 module SupermarketFour
   class Checkout
-    def initialize(prices)
-      @prices = prices
+    def initialize(rules)
+      @rules = rules
+      @log = ''
     end
 
-    def scan(*items)
+    def scan(items)
+      add_to_log items
     end
 
     def total
+      return 0 if log.empty?
     end
 
     private
 
-    def prices
-      @prices
+    def add_to_log(items)
+      @log += items
+    end
+
+    def log
+      @log
     end
   end
 end
